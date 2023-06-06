135b85cc2f8a4211146e184b5174220cbdce2b1e
augusto <augusdpmenezes@gmail.com>
2023-06-06 14:29:08 -0300

    troca de url

--- AngularUI/src/environments/environment.development.ts
+++ AngularUI/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
-    apiUrl: "https://localhost:7219/api",
+    production: true,
+    apiUrl: "https://sql-server-portfolio.azurewebsites.net/api",
 };
